362c1962740eb5f61b155c1d69e5632d2c066371
Vincent C. Mader <vincent.mader@icloud.com>
2023-04-20 21:10:33 +0200

    moved `gaussian_random` to `math/utils.js` directory

---
+++ server/static/js/math/utils.js
@@ -0,0 +1,7 @@
+export const gaussian_random = (mean = 0, stdev = 1) => { // TODO Move elsewhere.
+    let u = 1 - Math.random(); // Converting [0,1) to (0,1]
+    let v = Math.random();
+    let z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+    // Transform to the desired mean and standard deviation:
+    return z * stdev + mean;
+}
