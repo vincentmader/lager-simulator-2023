9f4171ac6c04a0a0492acba4dcc49b80e0aae604
Vincent C. Mader <vincent.mader@icloud.com>
2023-04-20 23:10:35 +0200

    passed world dimensions on to `FloorGrid`

--- server/static/js/data/floor_grid.js
+++ server/static/js/data/floor_grid.js
@@ -3,19 +3,18 @@ import {Rectangle} from "../math/rectangle.js";
 
 export class FloorGrid { // TODO Move definition elsewhere?
 
-    constructor() {
-        this.rectangles = this._rectangles();
+    constructor(dimensions) {
+        this.rectangles = this._rectangles(dimensions);
     }
 
-    _rectangles() {
-        let N = 100;
+    _rectangles(dimensions) {
         let W = 1,
             H = 1;
         let rectangles = [];
-        for (let idx = 0; idx < N; idx++) {
-            for (let jdx = 0; jdx < N; jdx++) {
-                let x = W * (idx - N / 2);
-                let y = H * (jdx - N / 2);
+        for (let idx = 0; idx < dimensions[0]; idx++) {
+            for (let jdx = 0; jdx < dimensions[1]; jdx++) {
+                let x = W * (idx - dimensions[0] / 2);
+                let y = H * (jdx - dimensions[1] / 2);
                 let position = new Position(x, y);
                 let rect = new Rectangle(position, [W, H]);
                 rectangles.push(rect);
--- server/static/js/data/world.js
+++ server/static/js/data/world.js
@@ -3,14 +3,15 @@ import {Position} from "../math/vector.js";
 import {FloorGrid} from "./floor_grid.js";
 
 export const dt = 1; // TODO Make dynamic.
+export const N = 100;
 
 export class World {
 
     constructor(people) {
         this.people = people
         this.fire = new Lagerfeuer(new Position(0, 0));
-        this.dimensions = [100, 100]; // TODO Make dynamic?
-        this.floor_grid = new FloorGrid();
+        this.dimensions = [N, N]; // TODO Make dynamic?
+        this.floor_grid = new FloorGrid(this.dimensions);
         // TODO ^ Is this the right location for storing the `FloorGrid`?
     }
 }
