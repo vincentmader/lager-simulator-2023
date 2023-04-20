ad1605d06d0c5e6549419cd5e6088bcdb9570bdf
Vincent C. Mader <vincent.mader@icloud.com>
2023-04-20 12:04:00 +0200

    minor: changed starting positions and added move-tasks

--- server/static/js/game.js
+++ server/static/js/game.js
@@ -11,10 +11,18 @@ import {Position} from "./position.js";
 export class Game {
 
     constructor() {
-        let person_1 = new Woelfling(new Position(100, 100));
-        let person_2 = new Jupfi(new Position(200, 100));
+        let person_1 = new Woelfling(new Position(-1, 1));
+        let person_2 = new Jupfi(new Position(-2, 2));
         let people = [person_1, person_2];
-        people[0].task_list.push(new MoveTask(new Position(200, 200)));
+
+        people[0].task_list.push(new MoveTask(new Position(+1, +1)));
+        people[0].task_list.push(new MoveTask(new Position(+1, -1)));
+        people[0].task_list.push(new MoveTask(new Position(-1, -1)));
+        people[0].task_list.push(new MoveTask(new Position(-1, +1)));
+        people[1].task_list.push(new MoveTask(new Position(+2, +2)));
+        people[1].task_list.push(new MoveTask(new Position(+2, -2)));
+        people[1].task_list.push(new MoveTask(new Position(-2, -2)));
+        people[1].task_list.push(new MoveTask(new Position(-2, +2)));
 
         this.canvas = new Canvas();
         this.world = new World(people);
