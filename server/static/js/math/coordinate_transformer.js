6da08d2fd03c1b1a0c3182df239d7f09a85ec730
Vincent C. Mader <vincent.mader@icloud.com>
2023-04-21 10:07:50 +0200

    fix: included z-coordinate into conversion from/to cartesian to/from isometric

--- server/static/js/math/coordinate_transformer.js
+++ server/static/js/math/coordinate_transformer.js
@@ -1,5 +1,6 @@
 import {Position} from "./vector.js";
 
+
 export class CoordinateTransformer {
 
     constructor(world, canvas) {
@@ -12,13 +13,15 @@ export class CoordinateTransformer {
     isometric_to_cartesian(position) {
         let x = (2 * position.y + position.x) / 2;
         let y = (2 * position.y - position.x) / 2;
-        return new Position(x, y);
+        let z = position.z;
+        return new Position(x, y, z);
     }
 
     cartesian_to_isometric(position) {
         let x = position.x - position.y;
         let y = (position.x + position.y) / 2;
-        return new Position(x, y);
+        let z = position.z;
+        return new Position(x, y, z);
     }
 
     world_to_canvas(position, zoom_level) {
@@ -31,7 +34,10 @@ export class CoordinateTransformer {
         // TODO Check if it shouldn't be p_max * window_scale_factor (inversion of canvas_to_world-formula?)
         // let window_scale_factor = 0.000765 * window.innerWidth;
         let x = p_max * (zoom_level * position.x - x_min) / (x_max - x_min);
-        let y = q_max * (zoom_level * position.y - y_max) / (y_min - y_max);
+        let y = q_max * (zoom_level * position.y - y_max) / (y_min - y_max) - 100 * position.z;
+        //                                                                     ^ TODO Scale accurately: 
+        //                                                                            Use projection factor, 
+        //                                                                            as well as world-to-canvas rescaling .
         return new Position(x, y);
     }
 
@@ -42,7 +48,7 @@ export class CoordinateTransformer {
         let y_max = this.world_width / 2;
         let p_max = this.canvas_width;
         let q_max = this.canvas_height;
-        let window_scale_factor = 0.000765 * window.innerWidth; // lollinger
+        let window_scale_factor = 0.000765 * window.innerWidth; // TODO Is this absolutely needed? Weirdly specific number...
         let x = (x_min + position.x / (p_max * window_scale_factor) * (x_max - x_min)) / zoom_level;
         let y = (y_max + position.y / (q_max * window_scale_factor) * (y_min - y_max)) / zoom_level;
         return new Position(x, y);
