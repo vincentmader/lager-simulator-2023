e54b17e2177f055b50a57f8b6d754f791a936f5b
Vincent C. Mader <vincent.mader@icloud.com>
2023-04-21 22:18:59 +0200

    reworked `Input` constructor & added `onclick` callback

--- server/static/js/io/input/inputs.js
+++ server/static/js/io/input/inputs.js
@@ -1,6 +1,16 @@
 class Input {
-    constructor(element_id) {
+    constructor(
+        element_id,
+        {
+            oninput = () => {},
+            onchange = () => {},
+            onclick = () => {},
+        }
+    ) {
         this.element = document.getElementById(element_id);
+        this.element.oninput = () => oninput(this.element.value);
+        this.element.onchange = () => onchange(this.element.value);
+        this.element.onclick = () => onclick(this.element.value);
     }
 }
 
@@ -11,15 +21,21 @@ export class Slider extends Input {
         {
             oninput = () => {},
             onchange = () => {},
+            onclick = () => {},
             min = 0,
             max = 100,
             step = 1,
             value = 50,
         }
     ) {
-        super(element_id);
-        this.element.oninput = () => oninput(this.element.value);
-        this.element.onchange = () => onchange(this.element.value);
+        super(
+            element_id,
+            {
+                oninput: oninput,
+                onchange: onchange,
+                onclick: onclick,
+            }
+        );
         this.element.min = min;
         this.element.max = max;
         this.element.step = step;
