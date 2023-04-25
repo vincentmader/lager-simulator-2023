5294c3acd9383a1f2e6556cce180cb4bb9f9f393
Vincent C. Mader <vincent.mader@icloud.com>
2023-04-25 21:55:33 +0200

    removed `RESOLUTION_FACTOR`

--- server/static/js/io/output/game_display.js
+++ server/static/js/io/output/game_display.js
@@ -1,18 +1,13 @@
 import {CheckBox, Slider} from "../input/inputs.js";
 
-const RESOLUTION_FACTOR = 4;
-
 export class GameDisplay {
 
     constructor(zoom_level) {
         this.element = document.getElementById("canvas-0");
         this.ctx = this.element.getContext('2d');
 
-        let W_internal = this.element.width;
-        let H_internal = W_internal;
-
-        this.W = W_internal * RESOLUTION_FACTOR;
-        this.H = H_internal * RESOLUTION_FACTOR;
+        this.W = window.innerWidth;
+        this.H = window.innerHeight;
 
         this.element.width = this.W;
         this.element.height = this.H;
