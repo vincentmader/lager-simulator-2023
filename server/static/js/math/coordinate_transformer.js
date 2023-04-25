7627e02c827dedd2c48b4644e25d37cb5e2e1e77
Vincent C. Mader <vincent.mader@icloud.com>
2023-04-25 22:01:17 +0200

    removed `window_scale_factor`

--- server/static/js/math/coordinate_transformer.js
+++ server/static/js/math/coordinate_transformer.js
@@ -30,8 +30,6 @@ export class CoordinateTransformer {
         let y_max = this.world_height / 2;
         let p_max = this.game_display_width;
         let q_max = this.game_display_height;
-        // TODO Check if it shouldn't be p_max * window_scale_factor (inversion of game_display_to_world-formula?)
-        // let window_scale_factor = 0.000765 * window.innerWidth;
         let x = p_max * (zoom_level * position.x - x_min) / (x_max - x_min);
         let y = q_max * (zoom_level * position.y - y_max) / (y_min - y_max) - 100 * position.z;
         //                                                                     ^ TODO Scale accurately: 
@@ -47,9 +45,8 @@ export class CoordinateTransformer {
         let y_max = this.world_width / 2;
         let p_max = this.game_display_width;
         let q_max = this.game_display_height;
-        let window_scale_factor = 0.000835 * window.innerWidth; // TODO Is this absolutely needed? Weirdly specific number...
-        let x = (x_min + position.x / (p_max * window_scale_factor) * (x_max - x_min)) / zoom_level;
-        let y = (y_max + position.y / (q_max * window_scale_factor) * (y_min - y_max)) / zoom_level;
+        let x = (x_min + position.x / p_max * (x_max - x_min)) / zoom_level;
+        let y = (y_max + position.y / q_max * (y_min - y_max)) / zoom_level;
         return new Position(x, y);
     }
 }
