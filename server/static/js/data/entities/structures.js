cc060c46c88759cad73e98370b518eda69b66848
Vincent C. Mader <vincent.mader@icloud.com>
2023-04-26 12:29:49 +0200

    fixed sprite location

--- server/static/js/data/entities/structures.js
+++ server/static/js/data/entities/structures.js
@@ -40,18 +40,24 @@ export class PfadiZelt extends Zelt {
 
 export class RoverZelt extends Zelt {
     constructor(position) {
-        super(position, [6, 4], "/img/sprites/sort/Isometriccampingtent/RIGHT_NO_OUTLINE.png");
+        super(position, [6, 4], "/img/sprites/structures/tents/RIGHT_NO_OUTLINE.png");
     }
 }
 
 export class LeiterJurte extends Zelt {
     constructor(position) {
-        super(position, [4, 6], "/img/sprites/sort/Isometriccampingtent/LEFT_NO_OUTLINE.png");
+        super(position, [4, 6], "/img/sprites/structures/tents/LEFT_NO_OUTLINE.png");
     }
 }
 
 export class Lagerfeuer extends Entity {
     constructor(position) {
         super(position, [2, 2]);
+    }
+}
+
+export class Tree extends Entity {
+    constructor(position, bounding_box_dims, texture) {
+        super(position, bounding_box_dims, texture);
     }
 }
