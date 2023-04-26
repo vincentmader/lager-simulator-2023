cd9fe4edf35c8e9ba690cfa2c30243d6e54e2c0e
Vincent C. Mader <vincent.mader@icloud.com>
2023-04-26 11:08:52 +0200

    fixed transform to accurately handle origin location on zoom

--- server/static/js/math/coordinate_transformer.js
+++ server/static/js/math/coordinate_transformer.js
@@ -24,15 +24,15 @@ export class CoordinateTransformer {
 
     world_to_game_display(position, zoom_level) {
         let canvas_origin = new Vector(this.game_display.width / 2, this.game_display.height / 2);
-        let x = canvas_origin.x + position.x * zoom_level + this.game_display.display_origin.x;
-        let y = canvas_origin.y + position.y * zoom_level - position.z + this.game_display.display_origin.y;
+        let x = canvas_origin.x + zoom_level * (position.x + this.game_display.display_origin.x);
+        let y = canvas_origin.y + zoom_level * (position.y + this.game_display.display_origin.y - position.z);
         return new Position(x, y);
     }
 
     game_display_to_world(position, zoom_level) {
         let canvas_origin = new Vector(this.game_display.width / 2, this.game_display.height / 2);
-        let x = (position.x - canvas_origin.x - this.game_display.display_origin.x) / zoom_level;
-        let y = (position.y - canvas_origin.y - this.game_display.display_origin.y) / zoom_level;
+        let x = (position.x - canvas_origin.x) / zoom_level - this.game_display.display_origin.x;
+        let y = (position.y - canvas_origin.y) / zoom_level - this.game_display.display_origin.y + position.z;
         return new Position(x, y);
     }
 }
